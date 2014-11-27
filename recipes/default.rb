4bd2b14ed8376398e4b182e8db0897e735234656
lukaszpio <lukaszpio@users.noreply.github.com>
2014-11-27 14:45:37 +0100

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,11 +5,7 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#Dodatkowy komentarz lokalny
-=======
 
-#web
->>>>>>> 647a0b6b377c2e8840fafbad7024dde8fcc0256b
 package 'httpd' do
   action :install
 end
